bb9976569cf87f4cef4796e412c7040afbade51c
harshitarav <harshitaravi96@gmail.com>
2025-07-15 10:25:22 -0500

    Time: 869 ms (90.84%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,5 @@
-SELECT contest_id, percentage FROM (
-SELECT contest_id, percentage, row_number() OVER(PARTITION BY percentage ORDER BY contest_id ASC) AS row_n FROM (
 SELECT * FROM (
-SELECT round((row_rank/total_users)*100,2) AS percentage, contest_id
+SELECT contest_id, round((row_rank/total_users)*100,2) AS percentage
 FROM (
 SELECT * FROM (
 SELECT contest_id, total_users, user_id, row_rank, rank() OVER(PARTITION BY contest_id ORDER BY row_rank DESC) AS row_num FROM (
@@ -10,6 +8,5 @@ SELECT contest_id, u.user_id, (SELECT COUNT(user_id) FROM Users) AS total_users
 FROM Users u JOIN Register r
 ON u.user_id = r.user_id
 ORDER BY contest_id) AS TAB1) AS TAB2) AS TAB3
-WHERE row_num = 1) AS TAB4) AS TAB5) AS TAB6
-ORDER BY percentage DESC, contest_id ASC)
-AS TAB7;
+WHERE row_num = 1) AS TAB4) AS TAB5
+ORDER BY percentage DESC, contest_id ASC;
