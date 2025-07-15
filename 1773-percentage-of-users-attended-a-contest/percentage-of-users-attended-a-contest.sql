9682566e5d25719a3e2cd5365fd565cc5ae9ee1a
harshitarav <harshitaravi96@gmail.com>
2025-07-15 10:21:30 -0500

    Time: 886 ms (85.14%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -10,7 +10,6 @@ SELECT contest_id, u.user_id, (SELECT COUNT(user_id) FROM Users) AS total_users
 FROM Users u JOIN Register r
 ON u.user_id = r.user_id
 ORDER BY contest_id) AS TAB1) AS TAB2) AS TAB3
-WHERE row_num = 1) AS TAB4
-ORDER BY contest_id) AS TAB5) AS TAB6
+WHERE row_num = 1) AS TAB4) AS TAB5) AS TAB6
 ORDER BY percentage DESC, contest_id ASC)
 AS TAB7;
